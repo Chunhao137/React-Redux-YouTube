d02c18af5c52eb6567247bdaafcecc655cd647b7
Chun Hao Wang <charleswang@Chuns-MacBook-Pro.local>
2016-03-20 00:19:15 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var path = require('path');
+var express = require('express');
+
+var app = express();
+var PORT = process.env.PORT || 8080
+
+
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html')
+});
+
+app.listen(PORT, function() {
+  console.log("Server is up and running on port: " + PORT)
+});
